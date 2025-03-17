0d9d3e4be91b7d900081aae403430dbf6e842b55
MicheleMarson <mitchmarson666@gmail.com>
2025-03-17 20:41:26 +0100

    fixed error message

--- handlers/errorHandler.js
+++ handlers/errorHandler.js
@@ -2,7 +2,7 @@ const errorHandle = (error, req, res, next) => {
   if(error){
     res.status(400).json({
       status: "failed",
-      error: error.message?error.message:error
+      message: error.message?error.message:error
     })
   }else{
     next()
